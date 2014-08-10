0e7a2b05cccce0b57d08a2a2b5d86a0ce9f94cf9
Victor Lin <bornstub@gmail.com>
2014-08-09 21:44:55 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rundocker',
-    version='1.0.1',
+    version='1.0.2',
     description=(
         'A script for running docker container process without worrying '
         'about dead container issue'
